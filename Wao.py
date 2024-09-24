ec2ce00fc31117962e77cfd0100619add187054d
audharr <155938558+audharr@users.noreply.github.com>
2024-09-24 14:36:59 -0500

    Update Wao.py

--- Wao.py
+++ Wao.py
@@ -1,12 +1,10 @@
-import subprocess   #allows for me to access the system and use commands 
+import subprocess   #allows for me to access the system and use commands, aka connect to my inout and output txts
 
 input_file = r"C:\Users\aharr\OneDrive\Desktop\325 Project\Prompts.txt"  #path to my prompts.txt aka the input for the questions to phi3
 output_file = r"C:\Users\aharr\OneDrive\Desktop\325 Project\output.txt"  #path to my output.txt aka the output from the phi to a file
  
 command = f'type "{input_file}" | ollama run phi3 > "{output_file}"' #assigning command to run phi3 and store the output inside of the created output.txt file
 
-subprocess.run(command, shell = True)   #executing the command given by me 
+subprocess.run(command, shell = True)   #executing the command given by me, running the run() function to invoke the subprocess
 
 print(f"Output is written to the {output_file}")    #printing out that the output was working properly and there were no issues, then can open the output.txt file to check that there was a response
-
-
