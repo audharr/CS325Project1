7b512a9cf0b77fdb3ae3bec94df49ec4790b7aa5
audharr <audharr@siue.edu>
2024-10-12 22:11:34 -0500

    Added required files

--- Wao.py
+++ Wao.py
@@ -8,5 +8,3 @@ command = f'type "{input_file}" | ollama run phi3 > "{output_file}"' #assigning 
 subprocess.run(command, shell = True)   #executing the command given by me 
 
 print(f"Output is written to the {output_file}")    #printing out that the output was working properly and there were no issues, then can open the output.txt file to check that there was a response
-
-
